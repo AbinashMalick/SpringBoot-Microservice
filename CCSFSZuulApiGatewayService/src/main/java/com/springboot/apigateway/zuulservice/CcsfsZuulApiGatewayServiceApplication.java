b5dd1da3de2105124d25a5887089d501a240980d
Avi <Avi@Homework>
2019-12-03 23:24:26 +0530

    Added all files in first commit

---
+++ CCSFSZuulApiGatewayService/src/main/java/com/springboot/apigateway/zuulservice/CcsfsZuulApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.apigateway.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcsfsZuulApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcsfsZuulApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ CCSFSZuulApiGatewayService/src/test/java/com/springboot/apigateway/zuulservice/CcsfsZuulApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.apigateway.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcsfsZuulApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
